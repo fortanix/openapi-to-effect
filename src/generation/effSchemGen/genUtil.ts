733798c899969a02dd633a717403537cabcbe04a
mkrause <maikel.krause@fortanix.com>
2024-05-25 16:53:18 +0200

    Update formatting of license comments.

--- src/generation/effSchemGen/genUtil.ts
+++ src/generation/effSchemGen/genUtil.ts
@@ -2,7 +2,8 @@
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 
 import { dedent } from 'ts-dedent';
 import { type OpenAPIV3_1 as OpenAPIV3 } from 'openapi-types';
